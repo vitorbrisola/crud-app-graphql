ee49f87d5c24c9c775e6dce0885323bee0b4e711
VitorBrisola <vitoraugusto199@gmail.com>
2019-12-11 20:35:40 -0300

    test code cleaning

--- app/src/pages/components/test.js
+++ app/src/pages/components/test.js
@@ -2,7 +2,6 @@ import React,{Component} from 'react';
 import {Card,Icon, Menu,Button} from 'semantic-ui-react';
 import './test.css'
 
-import QuestionInput from './question/input'
 import Question from './question'
 
 import QuestionsQuery from '../../relay/queries/questions';
@@ -28,123 +27,57 @@ export default class Test extends Component{
     loadQuestions = async () =>{
         await QuestionsQuery()
             .then(data => {
+                //const array = []
                 for(var item of data.questions){
-                    console.log(item.description)
+                    //array.push(new Question({id:item.id,reRender:this.reRender}))
+
                     this.setState({
                         questions: [...this.state.questions,new Question({id:item.id,reRender:this.reRender})]})
                 };
-	            if(data.questions){this.setState({questDisplayMode:'test'})}
+
+                if(data.questions.length === 0){
+                    this.setState({questions:[new Question({id:null,reRender:this.reRender})]})
+                }
+	            
             })
             .catch(err => console.log(err.message))
     }
 
     changeQuestion = (e,{name}) => {
-        if(this.state.questDisplayMode === 'test')  {
-            this.setState({curIndex: Number(name)})
-        }
+        this.setState({curIndex: Number(name)})
     }
 
-    /*setEmptyQuestion = () => {
-        // do noting when in edit mode
-        if(this.state.questDisplayMode !== 'normal'){return false}
-        this.setState({questDisplayMode:'add'})
-        var newQuestion = null
-        const index = this.state.questions.length
-        this.setState({questions:[...this.state.questions,newQuestion],curIndex:index})
-    }
-
-    addQuestion = async (description) => {
-        const newQuestion = new Question(null,description)
-        const array = [...this.state.questions]
-        if(array.length > 0){
-            array.pop()
-            await this.setState({questions:[...array,newQuestion]})
-        }else{
-            await this.setState({questions:[newQuestion],curIndex:0})
-        }
-        this.setState({questDisplayMode:'normal'})
-    }*/
-
-    setEmptyQuestion = async () => {
+    addQuestion = () => {
         const newQuestion = new Question({id:null,reRender:this.reRender})
         const array = [...this.state.questions]
         const index = array.length
-        await this.setState({questions:[...array,newQuestion],curIndex:index})
+        this.setState({questions:[...array,newQuestion],curIndex:index})
         //this.setState({questDisplayMode:'normal'})
     }
 
     deleteQuestion = async () => {
-        const index = this.state.curIndex;
-        console.log(index)
+        var  index = this.state.curIndex;
 	    //delete from server
-	    this.state.questions[index].delete()
+        this.state.questions[index].delete()
+        
         // deleting question locally
         var array = [...this.state.questions]
         array.splice(index,1)
-
+        if(index === array.length){index = index - 1}
+        await this.setState({questions:[...array], curIndex: index})
         // changing current question, add new for zero questions
         if(array.length === 0){
-            this.setEmptyQuestion()
-        }else if(index === array.length){
-            await this.setState({curIndex: (index-1)})
+            this.addQuestion()
         }
 
-        // deleting question locally
-        await this.setState({questions:[...array], deleteCounter:(this.state.deleteCounter+1)})
     }
 
-    editQuestion = () => {
-        this.setState({questDisplayMode:'edit'})
-    }
-
-    updateQuestion = async (newDescription) => {
-	    const index = this.state.curIndex;
-        this.state.questions[index].update(newDescription);
-        this.setState({questDisplayMode:'normal'})
-    }
-
-
-    newAnswer = () => {
-        const index = this.state.curIndex;
-        this.state.questions[index].setNewEmptyAnswer();
-        this.reRender()
-    }
 
     questionRender = (question) => {
-        if(this.state.questDisplayMode === 'add'){
+        if(this.state.questions.length === 0){
             // question adding  layout
             return(
-                <QuestionInput onClick={this.addQuestion}/>
-            )
-        }else if(this.state.questDisplayMode === 'edit'){
-            // question adding  layout
-            return(
-                <QuestionInput 
-                    value={this.state.questions[this.state.curIndex].description} 
-                    onClick={this.updateQuestion}/>
-            )
-        }else if(this.state.questDisplayMode === 'normal'){
-            // question visualization layout
-            return(
-                <div>
-                    <div className='editButtons'>
-			            <Button icon='pencil' color='blue' onClick={this.editQuestion}/>
-                        <Button icon='trash' color='red' onClick={this.deleteQuestion}/>
-                    </div>
-                    <Card
-                        fluid
-                        header={'Questão '+(this.state.curIndex+1).toString()}
-                        description={question.description}
-                    />
-                    <div>
-                        {this.state.questions[this.state.curIndex].answers.map((item,key) => {
-                            return (
-                                <div key={key} className='answers'>
-                                    {item.render(key,this.newAnswer)}
-                                </div>)
-                        })}
-                    </div>
-                </div>
+                <div>Loading...</div>
             )
         }else{
             return(this.state.questions[this.state.curIndex].render(this.state.curIndex))
@@ -177,7 +110,7 @@ export default class Test extends Component{
                         <Menu.Item
                             name='new-tab'
                             active={activeItem === 'new-tab'}
-                            onClick={this.setEmptyQuestion}
+                            onClick={this.addQuestion}
                         >
                         <Icon name='add' />
                             Nova Questão
